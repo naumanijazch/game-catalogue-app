8880ee40b1b76994455b91db65c9f43cbcd197e6
naumanijazch <naumanijaz@gosaas.io>
2024-07-02 10:26:06 +0500

    create-custom-hook-to-fetch-games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const FetchGames = async () => {
-      try {
-        const res = await apiClient.get<FetchGamesResponse>("/games");
-        console.log("Res: ", res);
-        setGames(res.data.results);
-        console.log("Successful");
-      } catch (error: any) {
-        setError(error.message);
-        console.log("Error: ", error.message);
-      }
-    };
-    FetchGames();
-  }, []);
-
+  const { games, error } = useGames(); //* custom hook
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+        const controller = new AbortController()
+      const FetchGames = async () => {
+        try {
+          const res = await apiClient.get<FetchGamesResponse>("/games", {signal: controller.signal});
+          console.log("Res: ", res);
+          setGames(res.data.results);
+          console.log("Successful");
+        } catch (error: any) {
+            if (error instanceof CanceledError) return;
+            setError(error.message);
+            console.log("Error: ", error.message);
+        }
+      };
+      FetchGames();
+      return ()=> {controller.abort}
+    }, []);
+
+    return {games, error}
+}
+
+export default useGames
