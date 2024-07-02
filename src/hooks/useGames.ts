4a9350adc5965caecb5e099285a8caff1cf4590c
naumanijazch <naumanijaz@gosaas.io>
2024-07-02 11:27:20 +0500

    show-loading-skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,18 +23,22 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
   
     useEffect(() => {
-        const controller = new AbortController()
+      const controller = new AbortController()
+      setIsLoading(true)
       const FetchGames = async () => {
         try {
           const res = await apiClient.get<FetchGamesResponse>("/games", {signal: controller.signal});
           console.log("Res: ", res);
           setGames(res.data.results);
+          setIsLoading(false)
           console.log("Successful");
         } catch (error: any) {
             if (error instanceof CanceledError) return;
             setError(error.message);
+            setIsLoading(false)
             console.log("Error: ", error.message);
         }
       };
@@ -42,7 +46,7 @@ const useGames = () => {
       return ()=> {controller.abort}
     }, []);
 
-    return {games, error}
+    return {games, error, isLoading}
 }
 
 export default useGames
