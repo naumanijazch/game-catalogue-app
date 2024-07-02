62b6e946f27063b1adb5222a71d73c75b5c74bbc
naumanijazch <naumanijaz@gosaas.io>
2024-07-02 15:54:57 +0500

    smoother-filter-by-genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -27,7 +27,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
+        {!isLoading && data.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
