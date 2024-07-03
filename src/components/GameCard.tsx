0daa250aaa8b3360abbadfb36fd34d45952130d6
naumanijazch <naumanijaz@gosaas.io>
2024-07-03 16:29:57 +0500

    static-card-size

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -6,6 +6,7 @@ import {
   HStack,
   Image,
   Spacer,
+  Stack,
 } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
@@ -19,10 +20,12 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card key={"md"} size={"md"}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Box whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+          <Heading fontSize="2xl">{game.name}</Heading>
+        </Box>{" "}
         <HStack justifyContent="space-between" marginTop={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
