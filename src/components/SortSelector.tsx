b435f50f8eeee638af1b8b4a1c8fa1c2fdbec553
naumanijazch <naumanijaz@gosaas.io>
2024-07-03 11:36:43 +0500

    build-sort-selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,33 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const SortSelector = () => {
+  //   const sortOrders = [
+  //     { value: "", label: "Relevance" },
+  //     { value: "-added", label: "Date added" },
+  //     { value: "name", label: "Name" },
+  //     { value: "-released", label: "Release date" },
+  //     { value: "-metacritic", label: "Popularity" },
+  //     { value: "-rating", label: "Average rating" },
+  //   ];
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by: "Relevance"
+      </MenuButton>
+      <MenuList>
+        {/* {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))} */}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
