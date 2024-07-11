1180d1095469872f01b06fb076d8293ad3efc4ff
naumanijazch <24100311@lums.edu.pk>
2024-07-11 11:02:32 +0500

    add react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import App from "./App";
 import "./index.css";
 
@@ -10,7 +12,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ChakraProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
